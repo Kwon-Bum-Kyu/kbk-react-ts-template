5311c8f5fc9363fc5eb3f2c6babd6dbb99b3c732
KBK <67410146+Kwon-Bum-Kyu@users.noreply.github.com>
2025-01-20 00:37:49 +0900

    feat: 프로젝트 초기화
    feat: 프로젝트 초기화

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,9 @@
 import "./App.css";
-import Header from "@/components/header.tsx";
-import Footer from "@/components/footer.tsx";
-import Slider from "@/components/silder.tsx";
-import TrendingPost from "@/components/trendingPost.tsx";
-import {
-  businessPosts,
-  businessSidePosts,
-  culturePosts,
-  cultureSidePosts,
-  lifeStylePosts,
-  lifeStyleSidePosts,
-  sliderPosts,
-  trendingPosts,
-  trendingSidePosts,
-} from "@/config/post.ts";
-import CulturePost from "@/components/culturePost.tsx";
-import BusniessPost from "@/components/businessPost.tsx";
-import LifeStylePost from "@/components/lifeStylePost.tsx";
 
 function App() {
   return (
     <>
-      <Header />
-      <main className="main">
-        <Slider sliders={sliderPosts} />
-        <TrendingPost posts={trendingPosts} sidePosts={trendingSidePosts} />
-        <CulturePost posts={culturePosts} sidePosts={cultureSidePosts} />
-        <BusniessPost posts={businessPosts} sidePosts={businessSidePosts} />
-        <LifeStylePost posts={lifeStylePosts} sidePosts={lifeStyleSidePosts} />
-        {/* Add more sections as needed */}
-      </main>
-      <Footer />
+      <main className="main"></main>
     </>
   );
 }
