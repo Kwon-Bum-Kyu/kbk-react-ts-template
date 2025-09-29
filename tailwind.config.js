e5dacbd98f1f0f8123b096049305a58552dfb0be
KBK <67410146+Kwon-Bum-Kyu@users.noreply.github.com>
2025-09-29 23:24:17 +0900

    feat: tailwind 4 마이그레이션

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,68 +1,7 @@
 /** @type {import('tailwindcss').Config} */
-const defaultTheme = require("tailwindcss/defaultTheme");
+
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {
-      fontFamily: { sans: ['"Open Sans"', ...defaultTheme.fontFamily.sans] },
-      fontSize: {
-        xs: "12px",
-        sm: "14px",
-        base: "16px",
-        lg: "18px",
-        xl: "20px",
-        "2xl": "24px",
-        "3xl": "32px",
-        "4xl": "40px",
-      },
-      lineHeight: {
-        16: "16px",
-        24: "24px",
-        32: "32px",
-        40: "40px",
-        48: "48px",
-        64: "64px",
-        72: "72px",
-      },
-      colors: {
-        blue: {
-          50: "#E1E9EF",
-          100: "#B5CBD6",
-          200: "#84A3BB",
-          300: "#527EA0",
-          400: "#2D63BB",
-          500: "#084777",
-          600: "#07406F",
-          700: "#663764",
-          800: "#042F5A",
-          900: "#022047",
-        },
-        gray: {
-          50: "#E7E7E6",
-          100: "#C2C2C1",
-          200: "#9A9A98",
-          300: "#72726F",
-          400: "#535350",
-          500: "#353531",
-          600: "#30302C",
-          700: "#282825",
-          800: "#22221F",
-          900: "#161613",
-        },
-        system: {
-          red: "#C2050B", // Error
-          green: "#07405F", // Success
-          white: "#FFFFFF", // Primary Background
-        },
-      },
-      spacing: { 16: "16px", 80: "80px" },
-      screens: {
-        mobile: "640px", // 모바일
-        tablet: "768px", // 태블릿
-        desktop: "1440px",
-        wide: "1920px",
-      },
-    },
-  },
+  theme: {},
   plugins: [],
 };
